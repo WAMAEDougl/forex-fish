12f0108f2ca7e374b962003ab0cd929fa51e018c
ForexFish Dev <dev@forexfish.ai>
2026-04-08 18:13:38 +0300

    fix: import MemoryModule into LearningModule for PersistentMemoryService

--- src/learning/learning.module.ts
+++ src/learning/learning.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SelfLearningService } from './self-learning.service';
+import { MemoryModule } from '../memory/memory.module';
 
 @Module({
+  imports: [MemoryModule],
   providers: [SelfLearningService],
   exports: [SelfLearningService],
 })
